7a74b4fe475eab4eb09bd1bdd914de5028daecf1
Alexey Nevsky <anwer.man@gmail.com>
2012-11-06 23:51:14 +0300

    Fixed images processing bugs

--- src/main/java/com/alexnevsky/web/capture/sites/GalyonkinContent.java
+++ src/main/java/com/alexnevsky/web/capture/sites/GalyonkinContent.java
@@ -102,62 +102,67 @@ public class GalyonkinContent {
 			for (Element div : divs) {
 				if (div.attr("id").startsWith("post-")) {
 					post = div;
+					break;
 				}
 			}
 
-			StringBuilder postTextSb = new StringBuilder(512);
-			Elements paragraphs = post.getElementsByTag("p");
+			if (post != null) {
+				StringBuilder postTextSb = new StringBuilder(512);
+				Elements paragraphs = post.getElementsByTag("p");
 
-			boolean isAddMoreTag = true;
+				boolean isAddMoreTag = true;
 
-			int pCount = 0;
-			for (Element i : paragraphs) {
-				pCount += 1;
-				postTextSb.append(i.outerHtml());
+				int pCount = 0;
+				for (Element i : paragraphs) {
+					pCount += 1;
+					postTextSb.append(i.outerHtml());
 
-				if (isAddMoreTag && pCount == 2) {
+					if (isAddMoreTag && pCount == 2) {
+						postTextSb.append(GlobalConstants.VIEW_MORE_TAG);
+						isAddMoreTag = false;
+					}
+				}
+
+				if (isAddMoreTag) {
 					postTextSb.append(GlobalConstants.VIEW_MORE_TAG);
 					isAddMoreTag = false;
 				}
+
+				postTextSb.append("<p style=\"font-size: 7pt; text-align:right;\">");
+				postTextSb.append("from <a href=\"");
+				postTextSb.append(postFullLink);
+				postTextSb.append("\" target=\"_blank\" style=\"color:gray\">galyorkin.com</a>");
+				postTextSb.append("</p>");
+
+				String postText = postTextSb.toString();
+
+				Story story = new Story(postTitle, postText, postFullLink);
+
+				Element tagBlock = post.getElementsByClass("meta").first();
+				Elements tags = tagBlock.getElementsByTag("a");
+				tags.remove(0);
+				StringBuilder tagsSb = new StringBuilder(64);
+				for (Element e : tags) {
+					tagsSb.append(e.text());
+					tagsSb.append(",");
+				}
+				tagsSb.append(this.TAGS);
+				tagsSb.append(",galyonkin");
+
+				story.addCommaSeparatedTags(tagsSb.toString());
+
+				Gallery gallery = this.processImages(container, postTitle);
+				if (gallery != null) {
+					List<Gallery> galleries = new ArrayList<Gallery>();
+					galleries.add(gallery);
+					story.setGalleries(galleries);
+				}
+
+				this.iGeneralService.save(story);
+
+			} else {
+				return;
 			}
-
-			if (isAddMoreTag) {
-				postTextSb.append(GlobalConstants.VIEW_MORE_TAG);
-				isAddMoreTag = false;
-			}
-
-			postTextSb.append("<p style=\"font-size: 7pt; text-align:right;\">");
-			postTextSb.append("from <a href=\"");
-			postTextSb.append(postFullLink);
-			postTextSb.append("\" target=\"_blank\" style=\"color:gray\">galyorkin.com</a>");
-			postTextSb.append("</p>");
-
-			String postText = postTextSb.toString();
-
-			Story story = new Story(postTitle, postText, postFullLink);
-
-			Element tagBlock = post.getElementsByClass("meta").first();
-			Elements tags = tagBlock.getElementsByTag("a");
-			tags.remove(0);
-			StringBuilder tagsSb = new StringBuilder(64);
-			for (Element e : tags) {
-				tagsSb.append(e.text());
-				tagsSb.append(",");
-			}
-			tagsSb.append(this.TAGS);
-			tagsSb.append(",galyonkin");
-
-			story.addCommaSeparatedTags(tagsSb.toString());
-
-			Gallery gallery = this.processImages(container, postTitle);
-			if (gallery != null) {
-				List<Gallery> galleries = new ArrayList<Gallery>();
-				galleries.add(gallery);
-				story.setGalleries(galleries);
-			}
-
-			this.iGeneralService.save(story);
-
 		} catch (MalformedURLException e) {
 			logger.error(e, e);
 		} catch (IOException e) {
@@ -168,38 +173,56 @@ public class GalyonkinContent {
 	}
 
 	private Gallery processImages(Element container, String title) throws MalformedURLException {
-		Gallery gallery = new Gallery(title, title);
+		Elements images = container.select("a[rel=lightbox]:has(img)");
+		Element imageElement = null;
+		String imageUrl = null;
 
-		Element imageElement = container.select("a[rel=lightbox]:has(img)").first();
-		String imageUrl = imageElement.attr("abs:href");
+		if (!images.isEmpty()) {
+			imageElement = images.first();
+			imageUrl = imageElement.attr("abs:href");
+		} else {
+			Elements divs = container.getElementsByTag("div");
+			for (Element div : divs) {
+				if (div.attr("class").startsWith("feature category-")) {
+					imageElement = div.getElementsByTag("img").first();
+					imageUrl = imageElement.attr("abs:src");
+					break;
+				}
+			}
+		}
 
-		long currentDate = new Date().getTime();
-		int count = 1;
+		if (imageUrl != null) {
+			long currentDate = new Date().getTime();
+			int count = 1;
 
-		String imageTitle = title;
+			String imageTitle = title;
 
-		String fileExtension = FileUtil.getPathExtension(imageUrl).toLowerCase();
+			String fileExtension = FileUtil.getPathExtension(imageUrl).toLowerCase();
 
-		String thumbPath = GlobalConstants.GALLERY_DIR_PATH + "games/" + currentDate + "_" + "images" + "/thumb/"
-				+ "games_image" + "_" + count + "-thumb." + fileExtension;
-		String thumbPathFull = GlobalConstants.DOMAIN_DIR_PATH + thumbPath;
+			String thumbPath = GlobalConstants.GALLERY_DIR_PATH + "games/" + currentDate + "_" + "images" + "/thumb/"
+					+ "games_image" + "_" + count + "-thumb." + fileExtension;
+			String thumbPathFull = GlobalConstants.DOMAIN_DIR_PATH + thumbPath;
 
-		String imagePath = GlobalConstants.GALLERY_DIR_PATH + "games/" + currentDate + "_" + "images" + "/"
-				+ "games_image" + "_" + count + "." + fileExtension;
-		String imagePathFull = GlobalConstants.DOMAIN_DIR_PATH + imagePath;
+			String imagePath = GlobalConstants.GALLERY_DIR_PATH + "games/" + currentDate + "_" + "images" + "/"
+					+ "games_image" + "_" + count + "." + fileExtension;
+			String imagePathFull = GlobalConstants.DOMAIN_DIR_PATH + imagePath;
 
-		Downloader.downloadFile(new URL(imageUrl), new File(imagePathFull));
+			Downloader.downloadFile(new URL(imageUrl), new File(imagePathFull));
 
-		// create thumb image
-		ImageUtil.resizeImageByWidth(new File(imagePathFull), new File(thumbPathFull), 200);
+			// create thumb image
+			ImageUtil.resizeImageByWidth(new File(imagePathFull), new File(thumbPathFull), 200);
 
-		Image image = new Image(imageTitle, imageTitle, imagePath);
-		image.setThumbPath(thumbPath);
-		image.setCategory(GlobalConstants.CATEGORY_GALYORKIN_GAMES);
+			Image image = new Image(imageTitle, imageTitle, imagePath);
+			image.setThumbPath(thumbPath);
+			image.setCategory(GlobalConstants.CATEGORY_GALYORKIN_GAMES);
 
-		gallery.addImage(image);
+			Gallery gallery = new Gallery(title, title);
+			gallery.addImage(image);
 
-		return gallery;
+			return gallery;
+		} else {
+			return null;
+		}
 	}
 
 	public boolean isStoryExist(String source) {
